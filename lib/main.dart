0707defee9a4112762f447dff967b3b8e14b0eb6
alexisdjgoyo <alexisdjesusgoyoflores@gmail.com>
2023-04-20 16:09:49 -0400

    custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_functions_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.blue
-      ),
-      home: const CounterScreen(),
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
+      home: const CounterFunctionsScreen(),
     );
   }
 }
